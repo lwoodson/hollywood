ce0a19f319cbbee9c7fe6db203d4356af18ee804
Lance Woodson <lance@webmaneuvers.com>
2014-05-04 06:16:22 -0500

    Changed datetimes of Movies to dates and exposed migrations to web
    Added a change to the core engine to allow its migrations to be
    exposed for running by the host app.

--- core/db/migrate/20140502091238_create_core_movies.rb
+++ core/db/migrate/20140502091238_create_core_movies.rb
@@ -2,9 +2,9 @@ class CreateCoreMovies < ActiveRecord::Migration
   def change
     create_table :core_movies do |t|
       t.string :title, null: false
-      t.datetime :production_start, null: false
-      t.datetime :production_finish
-      t.datetime :premiere
+      t.date :production_start, null: false
+      t.date :production_finish
+      t.date :premiere
       t.text :description
       t.timestamps
     end
--- core/lib/core/engine.rb
+++ core/lib/core/engine.rb
@@ -1,5 +1,19 @@
 module Core
   class Engine < ::Rails::Engine
     isolate_namespace Core
+
+    # HOWTO  This is a way to make your migrations available in your
+    # rails engine.  This has to be done in any engine defining new
+    # models.  This is from:
+    #
+    # http://pivotallabs.com/leave-your-migrations-in-your-rails-engines
+    #
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match(root.to_s)
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
